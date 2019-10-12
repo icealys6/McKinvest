1bb60e37790e69b8ba0490e63b42a7d5eef1eaea
icealys6 <icealys6@gmail.com>
2019-10-12 14:20:24 -0400

    second commit

---
+++ McKinvest/Controllers/BioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace McKinvest.Controllers
+{
+    public class BioController : Controller
+    {
+        // GET: Bio
+        public ActionResult Bio()
+        {
+            return View();
+        }
+    }
+}
---
+++ McKinvest/Controllers/MRIController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace McKinvest.Controllers
+{
+    public class MRIController : Controller
+    {
+        // GET: MRI
+        public ActionResult MRI()
+        {
+            return View();
+        }
+    }
+}
